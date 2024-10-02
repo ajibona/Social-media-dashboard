3351f8663c5a959ba10319909ad072a753e49d89
ajibona <39883461+ajibona@users.noreply.github.com>
2024-10-01 17:34:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const toogleElement = document.querySelector('.toogle-icon');
 const toogleCircleElement = document.querySelector('.toogle-circle');
 
+let lightMode = localStorage.getItem('switchon')
+
+
 toogleElement.addEventListener('click', ()=>{
   toogleCircleElement.classList.toggle('active');
+
+  lightMode = localStorage.getItem('lightMode');
+
+  if(lightMode === 'switchon'){
+    
+    document.body.classList.remove('lightmode')
+
+    localStorage.setItem('lightMode', null);
+
+  }else{
+
+    document.body.classList.add('lightmode')
+
+    localStorage.setItem('lightMode', 'switchon')
+  }
+ 
 })
